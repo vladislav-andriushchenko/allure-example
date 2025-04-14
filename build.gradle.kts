bc1bc86c36beb3d0d649c47c15b06fc85303318f
Artem Eroshenko <eroshenkoam@gmail.com>
2025-04-14 18:48:50 +0300

    update project to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("io.qameta.allure") version "2.9.4"
+    id("io.qameta.allure") version "2.12.0"
 }
 
 group = "io.eroshenkoam"
@@ -8,22 +8,22 @@ version = version
 
 allure {
     report {
-        version.set("2.18.1")
+        version.set("2.29.1")
     }
     adapter {
         autoconfigure.set(true)
         aspectjWeaver.set(true)
         frameworks {
             junit5 {
-                adapterVersion.set("2.18.1")
+                adapterVersion.set("2.29.1")
             }
         }
     }
 }
 
 tasks.withType(JavaCompile::class) {
-    sourceCompatibility = "${JavaVersion.VERSION_1_8}"
-    targetCompatibility = "${JavaVersion.VERSION_1_8}"
+    sourceCompatibility = "${JavaVersion.VERSION_21}"
+    targetCompatibility = "${JavaVersion.VERSION_21}"
     options.encoding = "UTF-8"
 }
 
@@ -45,9 +45,9 @@ repositories {
 }
 
 dependencies {
-    implementation("commons-io:commons-io:2.6")
-    implementation("io.qameta.allure:allure-java-commons:2.14.0")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-    implementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    implementation("commons-io:commons-io:2.15.1")
+    implementation("io.qameta.allure:allure-java-commons:2.29.1")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.12.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.12.2")
+    implementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
 }
